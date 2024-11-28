d81ee65bf4a5ccae9f315c73698f853660b96124
Meet Shiroya <meetshiroya14042000@gmail.com>
2024-11-28 13:20:57 +0530

    refactor: update feature branch for AI Agent rename

--- tests/aiagent.ts
+++ tests/aiagent.ts
@@ -3,11 +3,11 @@
 // import { Paragraph } from '@ckeditor/ckeditor5-paragraph';
 // import { Heading } from '@ckeditor/ckeditor5-heading';
 // import { ClassicEditor } from '@ckeditor/ckeditor5-editor-classic';
-// import AiAssist from '../src/aiassist.js';
+// import AiAgent from '../src/aiagent.js';
 
-// describe( 'AiAssist', () => {
+// describe( 'AiAgent', () => {
 // 	it( 'should be named', () => {
-// 		expect( AiAssist.pluginName ).to.equal( 'AiAssist' );
+// 		expect( AiAgent.pluginName ).to.equal( 'AiAgent' );
 // 	} );
 
 // 	describe( 'init()', () => {
@@ -22,10 +22,10 @@
 // 					Paragraph,
 // 					Heading,
 // 					Essentials,
-// 					AiAssist
+// 					AiAgent
 // 				],
 // 				toolbar: [
-// 					'aiAssistButton'
+// 					'aiAgentButton'
 // 				]
 // 			} );
 // 		} );
@@ -35,18 +35,18 @@
 // 			return editor.destroy();
 // 		} );
 
-// 		it( 'should load AiAssist', () => {
-// 			const myPlugin = editor.plugins.get( 'AiAssist' );
+// 		it( 'should load AiAgent', () => {
+// 			const myPlugin = editor.plugins.get( 'AiAgent' );
 
-// 			expect( myPlugin ).to.be.an.instanceof( AiAssist );
+// 			expect( myPlugin ).to.be.an.instanceof( AiAgent );
 // 		} );
 
 // 		it( 'should add an icon to the toolbar', () => {
-// 			expect( editor.ui.componentFactory.has( 'aiAssistButton' ) ).to.equal( true );
+// 			expect( editor.ui.componentFactory.has( 'aiAgentButton' ) ).to.equal( true );
 // 		} );
 
 // 		it( 'should add a text into the editor after clicking the icon', () => {
-// 			const icon = editor.ui.componentFactory.create( 'aiAssistButton' );
+// 			const icon = editor.ui.componentFactory.create( 'aiAgentButton' );
 
 // 			expect( editor.getData() ).to.equal( '' );
 

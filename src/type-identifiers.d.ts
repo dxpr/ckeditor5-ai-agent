8566468d814e419669341f4e76472ca227394a57
Jurriaan Roelofs <jur@dxpr.com>
2024-11-28 08:12:31 +0100

    chore: remove legacy models

--- src/type-identifiers.d.ts
+++ src/type-identifiers.d.ts
@@ -1,4 +1,4 @@
-export type AiModel = 'gpt-3' | 'gpt-3.5-turbo' | 'gpt-4' | 'gpt-4-turbo' | 'gpt-4o' | 'gpt-4o-mini' | 'kavya-m1';
+export type AiModel = 'gpt-3.5-turbo' | 'gpt-4o' | 'gpt-4o-mini' | 'kavya-m1';
 export interface MarkdownContent {
     content: string;
     url: string;
--- src/type-identifiers.ts
+++ src/type-identifiers.ts
@@ -1,9 +1,6 @@
 // types
 export type AiModel =
-    'gpt-3' |
     'gpt-3.5-turbo' |
-    'gpt-4' |
-    'gpt-4-turbo' |
     'gpt-4o' |
     'gpt-4o-mini' |
     'kavya-m1';

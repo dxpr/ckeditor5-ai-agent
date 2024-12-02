8a58d1b5c217e47b42c837d119c4aa81b4b6835d
Meet Shiroya <meetshiroya14042000@gmail.com>
2024-12-02 13:36:00 +0530

    Add CKEditor setup for extractEditorContent tests to validate functionality

--- tests/util/text-utils.test.ts
+++ tests/util/text-utils.test.ts
@@ -3,6 +3,8 @@ import {
 	removeLeadingSpaces,
 	extractEditorContent
 } from '../../src/util/text-utils.js';
+import { ClassicEditor } from '@ckeditor/ckeditor5-editor-classic';
+import { Paragraph } from '@ckeditor/ckeditor5-paragraph';
 
 describe( 'text-utils', () => {
 	describe( 'removeLeadingSpaces', () => {
@@ -13,9 +15,27 @@ describe( 'text-utils', () => {
 	} );
 
 	describe( 'extractEditorContent', () => {
+		let domElement: HTMLElement, editor: ClassicEditor;
+
+		beforeEach( async () => {
+			domElement = document.createElement( 'div' );
+			document.body.appendChild( domElement );
+
+			editor = await ClassicEditor.create( domElement, {
+				plugins: [
+					Paragraph
+				]
+			} );
+		} );
+
+		afterEach( () => {
+			domElement.remove();
+			return editor.destroy();
+		} );
+
 		it( 'should extract content with correct size', () => {
 			const input = 'This is a test sentence. Another sentence here.';
-			const result = extractEditorContent( input, 20 );
+			const result = extractEditorContent( input, 20, false, editor );
 			expect( result.length ).to.be.lessThanOrEqual( 20 * 4 );
 		} );
 	} );

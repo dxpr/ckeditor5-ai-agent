e56f4670d6a51900cf0b9dc6e505134d1cd10329
Jurriaan Roelofs <jur@dxpr.com>
2024-12-20 14:17:34 +0100

    published 1.0.0-beta8

--- src/index.d.ts
+++
@@ -1,5 +0,0 @@
-import './augmentation.js';
-export { default as AiAgent } from './aiagent.js';
-export declare const icons: {
-    ckeditor: string;
-};
--- src/util/default-rules.js
+++
@@ -1,8 +0,0 @@
-import { getAllowedHtmlTags } from './html-utils.js';
-import defaultRulesJson from '../config/default-rules.json';
-export function getDefaultRules(editor) {
-    // Replace the placeholder in htmlFormatting with actual allowed tags
-    const rules = { ...defaultRulesJson };
-    rules.htmlFormatting = rules.htmlFormatting.replace('${getAllowedHtmlTags(editor).join(\', \')}', getAllowedHtmlTags(editor).join(', '));
-    return rules;
-}

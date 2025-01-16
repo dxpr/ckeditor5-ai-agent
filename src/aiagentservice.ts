73791826d2af5318fb6ffdc073fdd295942a6f93
Jurriaan Roelofs <jur@dxpr.com>
2025-01-16 11:28:25 +0100

    implement_allowed_tags_property

--- src/aiagentservice.ts
+++ src/aiagentservice.ts
@@ -8,6 +8,7 @@ import { ButtonView } from 'ckeditor5/src/ui.js';
 import { env } from 'ckeditor5/src/utils.js';
 import { ALL_MODERATION_FLAGS, MODERATION_URL } from './const.js';
 import { getErrorMessages } from './util/translations.js';
+import { getAllowedHtmlTags } from './util/html-utils.js';
 
 export default class AiAgentService {
 	private editor: Editor;
@@ -284,7 +285,10 @@ export default class AiAgentService {
 					temperature: this.temperature,
 					max_tokens: this.maxTokens,
 					stop: this.stopSequences,
-					stream: true
+					stream: true,
+					extra_body: {
+						allowed_html_tags: getAllowedHtmlTags( this.editor )
+					}
 				} ),
 				signal: controller.signal
 			} );

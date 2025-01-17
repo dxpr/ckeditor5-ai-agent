71507a9633fd34366c2c0ab7a994ca491af9201b
Jurriaan Roelofs <jur@dxpr.com>
2025-01-17 08:51:52 +0100

    beta18

--- src/index.js
+++
@@ -1,6 +0,0 @@
-import ckeditor from './../theme/icons/ckeditor.svg';
-import './augmentation.js';
-export { default as AiAgent } from './aiagent.js';
-export const icons = {
-    ckeditor
-};
--- src/util/url-utils.d.ts
+++
@@ -1,8 +0,0 @@
-/**
- * Fetches the content of a given URL and returns it as a string.
- *
- * @param url - The URL to fetch content from.
- * @returns A promise that resolves to the fetched content as a string.
- * @throws Will throw an error if the URL is invalid or if the fetch fails.
- */
-export declare function fetchUrlContent(url: string): Promise<string>;

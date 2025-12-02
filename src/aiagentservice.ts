5e3087788ad4344fbea380076549701f4b218502
jjroelofs <904576+jjroelofs@users.noreply.github.com>
2025-12-02 09:17:39 +0100

    fix: only send providers param when using default model (kavya-m1) (#167)
    The providers parameter should only be sent to the DXAI endpoint when
    using the default model (kavya-m1). This aligns with the fix applied
    to DXPR Builder in commit 4a78aee60.
    
    Closes #166
    
    Co-authored-by: Jurriaan Roelofs <jur@dxpr.com>

--- src/aiagentservice.ts
+++ src/aiagentservice.ts
@@ -299,8 +299,8 @@ export default class AiAgentService {
 					editor: this.editor
 				};
 
-				// Add providers if engine is dxai and providers is set
-				if ( this.aiEngine === 'dxai' && this.providers ) {
+				// Add providers if engine is dxai and model is kavya-m1 (default model)
+				if ( this.aiEngine === 'dxai' && this.aiModel === 'kavya-m1' && this.providers ) {
 					config.providers = this.providers;
 				}
 

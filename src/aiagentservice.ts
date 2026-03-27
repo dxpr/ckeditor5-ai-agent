14da4cfbaf69adfbf8150ebb2e0ae1875d8a6010
Jurriaan Roelofs <jur@dxpr.com>
2026-03-27 12:31:31 +0100

    style: correct tab depth in streaming update block

--- src/aiagentservice.ts
+++ src/aiagentservice.ts
@@ -406,13 +406,13 @@ export default class AiAgentService {
 		let isFirstChunk = true;
 		let contentBuffer = '';
 		let generatedContent = '';
-			const updateInterval = 1000 / this.writesPerSecond; // Calculate interval in ms
+		const updateInterval = 1000 / this.writesPerSecond; // Calculate interval in ms
 
-			const updateContent = async () => {
-				if ( contentBuffer ) {
-					await this.processContentHelper.updateContent( contentBuffer, blockID );
-				}
-			};
+		const updateContent = async () => {
+			if ( contentBuffer ) {
+				await this.processContentHelper.updateContent( contentBuffer, blockID );
+			}
+		};
 
 		const updateContentTimer = setInterval( updateContent, updateInterval );
 		try {
